452dc279b509d17b2c1c783ada75c44fda033855
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2024-06-10 21:07:24 +0530

    Add files via upload

---
+++ D1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 19:37:28 2024
+
+@author: anush
+"""
+
+#Printing to console
+print("Hello World")
+
+print("Welcome to the band name generator\nWhich city did you grow up in?")
+city=input()
+print("What is the name of your pet")
+pet=input()
+
+print(pet+" "+city)
