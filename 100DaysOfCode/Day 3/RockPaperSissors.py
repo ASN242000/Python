1c3ddf28bcbdd58448e6de7170670324d3c2d615
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2026-03-07 20:32:05 +0530

    Random Module

--- 100DaysOfCode/Day 3/RockPaperSissors.py
+++ 100DaysOfCode/Day 3/RockPaperSissors.py
@@ -27,20 +27,21 @@ sissors = '''
 image = [rock,paper,sissors]
 player_choice = int(input("What do you choose? Type 0 for rock, 1 for paper, 2 for scissors "))
 computer_choice = random.randint(0,2)
-print(image[player_choice])
-print(image[computer_choice])
+if 3 > player_choice >= 0:
+    print(image[player_choice])
+    print(image[computer_choice])
 
 if player_choice == 0 and computer_choice == 2:
     print("You Win")
 elif player_choice == 1 and computer_choice == 0:
     print("You Win")
-elif player_choice == 1 and computer_choice == 2:
-    print("You Win")
 elif player_choice == 2 and computer_choice == 1:
+    print("You Win")
+elif player_choice == 1 and computer_choice == 2:
     print("You Win")
 elif player_choice == computer_choice:
     print("Draw")
-elif player_choice <0 or player_choice >2:
+elif player_choice < 0 or player_choice > 2:
     print("You entered an invalid input")
 else:
     print("You lose!")
